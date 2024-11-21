4938eabdcce5eb54e60e9388f7d707cdc0b7affa
rcs19 <84814947+rcs19@users.noreply.github.com>
2024-11-21 16:36:39 +0000

    Created separate script for determining first peak dominated by noise

---
+++ src/noise.py
@@ -0,0 +1,32 @@
+from scipy import signal
+import numpy as np
+import matplotlib.pyplot as plt # Matplotlib plotting library
+
+file = np.loadtxt("savedata/1000_12.566370614359172_20.txt", delimiter=",")
+xdata = np.array(file[0])
+ydata = np.array(file[1])
+
+def Get_Signal_and_Noise():
+    # Make a semilog plot to see exponential damping
+    plt.figure()
+
+    # (Scatter) Plotting the peaks of the first harmonic 
+    peaks_xindex, _ = signal.find_peaks(ydata)
+    # iteratres through peaks, if next peak is larger than previous then remove peak. 
+    peaks_value = ydata[peaks_xindex]
+    for i, val in enumerate(peaks_value):
+        if peaks_value[i+1]>peaks_value[i]: # Compares if next peak is greater than this peak
+            last_signal = peaks_xindex[i]   # Last peak before the signal is dominated by noise
+            break
+        else:
+            last_signal = peaks_xindex[-1]  # If there is no noise
+        
+
+    plt.plot(xdata, ydata)
+    plt.xlabel("Time [Normalised]")
+    plt.ylabel("First harmonic amplitude [Normalised]")
+    plt.yscale('log')    
+    plt.ioff() # This so that the windows stay open
+    plt.show()
+
+Get_Signal_and_Noise()
