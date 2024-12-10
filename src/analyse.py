be74225d8f2de7d9dde1cde5dd6665c9dce11e6d
rcs19 <ryan.saputil@gmail.com>
2024-12-10 10:50:43 +0000

    added parallel run simulation function

--- src/analyse.py
+++ src/analyse.py
@@ -1,21 +1,29 @@
 from epic1d import *
 from noise import *
+from joblib import Parallel, delayed
 import pandas as pd
 import os
 
 def generate():
-    npart_list = [1000,2000,3000,4000,5000,7500,10000,20000,30000,50000,75000,100000]
+    npart_list = [1000,2000,3000,4000,5000,7500,10000,20000,30000,50000,75000,100000,125000,150000,175000,200000]
     for npart in npart_list:
         print(f"npart={npart}")
         Run_and_Save(npart=npart, ncells=20)
 
+def generate_parallel(n_jobs=4):
+    ncell_list = arange(10,201,10)
+    timestart=time()
+    Parallel(n_jobs=n_jobs)(delayed(Run_and_Save)(npart=5000, ncells=ncells) for ncells in ncell_list)
+    timeend=time()
+    print(f"Total Job Runtime (n_jobs={n_jobs}): {timeend-timestart}s")
+
 def save_csv():
-    directory = 'savedata/session2_varying_npart_05-12-2024'
+    directory = 'savedata/ncell_10-200'
 
     # Dataframe (Table.csv) format
     columns = ['npart','L','ncells','runtime','damp', 'damp_std', 'noise_level', 'omega', 'omega_std']
     rows = []
-
+    
     for filename in os.listdir(directory):
         try:
             filepath = os.path.join(directory, filename).replace('\\','/')   # example string: 'data/P1_B1_Idefault_Ndefault.mat'
@@ -33,12 +41,12 @@ def save_csv():
         
         # Building one row in table.csv
         row = [npart, cell_length, ncells, runtime]
-        row.extend(Signal_vs_Noise(filepath, show_plot=False))
-        row.extend(Find_Frequency(filepath, show_plot=False))
+        row.extend(Signal_vs_Noise(filepath, show_plot=False))  # Add damping rate, damping rate error, and noise level
+        row.extend(Find_Frequency(filepath, show_plot=False))   # Add angular frequency and its error
         rows.append(row)
         
     df = pd.DataFrame(rows, columns=columns)
-    df = df.sort_values(by='npart',ignore_index=True)
-    df.to_csv("variations_ncells_10-to-200.csv", index=False)  
+    df = df.sort_values(by=['npart','ncells'],ignore_index=True)
+    df.to_csv("ncell_10-200.csv", index=False)  
 
 save_csv()
